a179049681d32b0ac2fe4825d02d91c95211bbba
Rehan-sarwar <rehan232167@gmail.com>
2023-08-01 11:34:17 +0500

    message

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+
+      },
+      colors: {
+        'regal-blue': 'rgba(5, 180, 255, 1)',
+      },
+
+    },
+  },
+  plugins: [],
+} 
